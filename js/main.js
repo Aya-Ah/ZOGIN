901911f1cc91ddd17ced07e109b40444c436b584
Aya-Ah <57809558+Aya-Ah@users.noreply.github.com>
2020-06-03 16:47:32 +0300

    testimonial slideshow but  without  transition yet

--- js/main.js
+++ js/main.js
@@ -77,8 +77,27 @@ function closeAllSelect(elmnt) {
     }
   }
 }
-
 /* If the user clicks anywhere outside the select box,
 then close all select boxes: */
 document.addEventListener("click", closeAllSelect);
 
+//testimonial slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("testimonial-item");
+  var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
+}
